998924f592c8926bf42c80d475cac20d86e2f079
Mogo <cy1mxc@outlook.com>
2021-09-08 10:58:03 +0800

    功能：购物车页面

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import mutations from './mutations'
+import actions from './actions'
+import getters from './getters'
+
 Vue.use(Vuex)
+const state = {
+  //购物车列表
+  goodLists: [],
+  //购物车中商品件数
+  goodsCount: 0,
+  //购物车中每个商品的勾选情况
+  YESorNo: [],
+  //记录购物车全选框的状态
+  allPointIsShow: true
+}
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state,
+  mutations,
+  getters,
+  actions,
   modules: {}
 })
--- src/views/shopcart/Shopcart.vue
+++ src/views/shopcart/Shopcart.vue
@@ -1,7 +1,30 @@
 <template>
-  <div class="shopcart_box"></div>
+  <div class="shopcart_box">
+    <nav-bar class="nav-bar">
+      <div slot="center">购物车&nbsp;({{ $store.state.goodsCount }})</div>
+    </nav-bar>
+    <scroll class="scroll" :probeType="0" :pull-up-load="false" ref="scroll">
+      <contents @webupdate="webupdate"></contents>
+    </scroll>
+    <cart-tab-bar class="cart-tab-bar"></cart-tab-bar>
+  </div>
 </template>
 
+<script>
+import NavBar from '../../components/common/navbar/NavBar.vue'
+import Scroll from '../../components/common/scroll/Scroll.vue'
+import CartTabBar from './children/CartTabBar.vue'
+import Contents from './children/Contents.vue'
+export default {
+  components: { NavBar, Scroll, Contents, CartTabBar },
+  methods: {
+    //监听购物车页面刷新，来刷新滚动距离
+    webupdate() {
+      this.$refs.scroll.scroll.refresh()
+    }
+  }
+}
+</script>
 <style scoped>
 .shopcart_box {
   height: 100%;
